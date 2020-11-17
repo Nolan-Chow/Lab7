cda8d45cba48065a87c8b8434eb597f1e88d9133
Nolan <nolan.chow446@gmail.com>
2020-11-17 14:07:58 -0800

    fixed JavaScript error

--- scipts/script.js
+++ scipts/script.js
@@ -1,14 +1,14 @@
 let courseList = [
     course1 = {
-        code: 1515,
+        code: "ACIT 1515",
         name: 'Scripting for IT'
     },
     course2 = {
-        code: 1620,
+        code: "ACIT 1620",
         name: 'Fundamental Web Technologies'
     },
     course3 = {
-        code: 1630,
+        code: "ACIT 1630",
         name: 'Database Systems'
     }
 ];
@@ -18,17 +18,18 @@ do {
     user_code = prompt('Enter a course code.')
 } while (isNaN(Number(user_code)) || user_code.length !== 4);
 for (let course of courseList) {
-    if (Number(user_code) === course.code) {
-        console.log(`Yes I am taking the course: ACIT ${user_code} - ${course.name}`);
+  console.log(course.code.substring(5,9));
+    if (user_code === course.code.substring(5,9)) {
+        console.log(`Yes I am taking the course: ${course.code} - ${course.name}`);
         incourseList = true;
     }
 };
 if (!incourseList) {
     courseList.push(
         course4 = {
-            code: Number(user_code),
+            code: "ACIT " + user_code,
             name: null
         }
-    );
-    console.log('Course successfully added')
-};
+      );
+  console.log('Course successfully added')
+}
